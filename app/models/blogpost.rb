98494ebadbc7963ffdf03449db5d7be2b0e796c3
Robot-Oleg <kozelolezhka@gmail.com>
2021-09-28 16:12:25 +0300

    add masks to blogpost

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -7,7 +7,12 @@ class Blogpost < ActiveRecord::Base
   has_many :ratings
   has_one :category
   has_one :region
+  has_one_attached :main_image
   acts_as_taggable_on :tags
-  enum status: { unactive: 0, active: 1, published: 2, archive: 3}
+  enum status: { unactive: 0, active: 1, published: 2, archived: 3 }
+  enum mask: {  all_can_see: 0,
+                only_title_and_description: 1,
+                only_title: 2,
+                hide_from_unregistered: 3 }
 
 end
